a77011634405ea3e1d94061ca9f9b79073568488
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-16 19:58:09 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroIntroducido = Integer.parseInt(System.console().readLine("Introduzca un número: "));
+
+        int ultimoDigito = numeroIntroducido % 10;
+        int restoDelNumero = numeroIntroducido / 10;
+
+        int multiplicador = 1;
+        int aux = restoDelNumero;
+
+        while (aux != 0) {
+            multiplicador *= 10;
+            aux /= 10;
+        }
+
+        int numeroFinal = (ultimoDigito * multiplicador) + restoDelNumero;
+
+        System.out.println("El número resultado es: " + numeroFinal);
     }
 }
